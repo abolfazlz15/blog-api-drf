4ed5844a22a21c8f763923e65e382a3291465f8b
Abolfazl <abolfazlzaker1234@gmail.com>
2023-01-17 00:28:01 +0330

    add test for ArticleUpdateView authorized

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,12 +1,13 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+# from django.test import RequestFactory
 
 from accounts.models import User
 from blog.api import serializers
 from blog.models import Article, Category, Tag
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from blog.api.views import ArticleUpdateView
 
 class ArticleListViewTestCase(APITestCase):
     def setUp(self):
@@ -64,3 +65,26 @@ class ArticleDetailViewTestCase(APITestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+
+class ArticleUpdateViewTest(APITestCase):
+    def setUp(self):
+        # Create test user
+        self.user = User.objects.create_user(
+            email='test@gmail.com', full_name='test', password='test1234')        
+        self.refresh = RefreshToken.for_user(self.user)
+        self.token = str(self.refresh.access_token)
+        self.category = Category.objects.create(title='Test Category')
+        self.tag = Tag.objects.create(title="tag")
+        self.article = Article.objects.create(title='Test Article', author=self.user, category=self.category, status=True)
+
+    def test_update_article_authorized(self):
+        url = reverse('blog:article-update', args=(self.article.id,))
+        new_data = {
+            'title': 'test update',
+            'text': 'this is test article update',
+        }
+        response = self.client.put(url, data=new_data, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
