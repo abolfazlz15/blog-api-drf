c6d9e39a1850d7efba1bfecca00743bc74163308
Abolfazl <abolfazlzaker1234@gmail.com>
2023-01-18 01:38:32 +0330

    add test_get_article_update_unauthorized

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,13 +1,13 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-# from django.test import RequestFactory
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.models import User
 from blog.api import serializers
-from blog.models import Article, Category, Tag
-from rest_framework_simplejwt.tokens import RefreshToken
 from blog.api.views import ArticleUpdateView
+from blog.models import Article, Category, Tag
+
 
 class ArticleListViewTestCase(APITestCase):
     def setUp(self):
@@ -54,7 +54,8 @@ class ArticleDetailViewTestCase(APITestCase):
 
     def test_get_article_detail_authorized(self):
         url = reverse('blog:article-detail', kwargs={'pk': self.article.id})
-        response = self.client.get(url, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = self.client.get(
+            url, HTTP_AUTHORIZATION=f'Bearer {self.token}')
         serializer = serializers.ArticleDetailSrializer(instance=self.article)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -67,24 +68,31 @@ class ArticleDetailViewTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-
 class ArticleUpdateViewTest(APITestCase):
     def setUp(self):
         # Create test user
         self.user = User.objects.create_user(
-            email='test@gmail.com', full_name='test', password='test1234')        
+            email='test@gmail.com', full_name='test', password='test1234')
         self.refresh = RefreshToken.for_user(self.user)
         self.token = str(self.refresh.access_token)
         self.category = Category.objects.create(title='Test Category')
         self.tag = Tag.objects.create(title="tag")
-        self.article = Article.objects.create(title='Test Article', author=self.user, category=self.category, status=True)
-
-    def test_update_article_authorized(self):
-        url = reverse('blog:article-update', args=(self.article.id,))
-        new_data = {
+        self.article = Article.objects.create(
+            title='Test Article', author=self.user, category=self.category, status=True)
+        self.new_data = {
             'title': 'test update',
             'text': 'this is test article update',
         }
-        response = self.client.put(url, data=new_data, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_update_article_authorized(self):
+        url = reverse('blog:article-update', args=(self.article.id,))
+
+        response = self.client.put(
+            url, data=self.new_data, HTTP_AUTHORIZATION=f'Bearer {self.token}')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_get_article_update_unauthorized(self):
+        url = reverse('blog:article-update', args=(self.article.id,))
+
+        response = self.client.put(url, data=self.new_data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
