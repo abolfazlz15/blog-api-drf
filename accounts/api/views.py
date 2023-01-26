87b7d8589827d1bee3270b882728a41813429f88
Abolfazl <abolfazlzaker1234@gmail.com>
2023-01-27 03:11:36 +0330

    optimize UserRegisterView and CheckOtpCodeView & edit OTP class (Refactoring)

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,18 +1,17 @@
 from random import randint
 
-from accounts.api import serializers
-from accounts.models import OTPCode, User
 from django.core.cache import cache
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils import timezone
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from rest_framework.decorators import action
-from rest_framework import viewsets
+from accounts.api import serializers
+from accounts.models import OTPCode, User
 from accounts.otp_service import OTP
 
 
@@ -33,25 +32,14 @@ class UserRegisterView(APIView):
         serializer = serializers.UserRegisterSerializer(data=request.data)
 
         if serializer.is_valid():
-            randcode = randint(1000, 9999)
             cd = serializer.validated_data
+            otp_service = OTP()
+            otp_service.generate_otp(cd['email'])
             cache.set(key='register', value={
-                'email': cd['email'], 'password': cd['password'], 'full_name': cd['full_name'], 'code': randcode},
+                'email': cd['email'], 'password': cd['password'], 'full_name': cd['full_name']},
                       timeout=300)
             print(cache.get(key='register'))
-            OTPCode.objects.create(
-                code=randcode, email=cd['email'], password=cd['password'])
 
-            mail_subject = 'فعال سازی اکانت'
-            message = render_to_string('accounts/active_email.html', {
-                'user': cd['email'],
-                'code': randcode,
-            })
-            to_email = cd['email']
-            email = EmailMessage(
-                mail_subject, message, to=[to_email]
-            )
-            email.send()
             return Response({'email': cd['email'], 'result': 'email sended'}, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
 
@@ -62,22 +50,18 @@ class CheckOtpCodeView(APIView):
     def post(self, request):
         data = cache.get(key='register')
         serializer = serializers.GetOtpSerializer(data=request.data)
-        otp = OTPCode.objects.get(code=data['code'], email=data['email'])
-
-        expiration_date = otp.expiration_date + timezone.timedelta(minutes=2)
-        if expiration_date < timezone.now():
-            otp.delete()
-            return Response({'error': 'your code is expire'}, status=status.HTTP_406_NOT_ACCEPTABLE)
-
+        otp_obj = OTP()
+        if data is None:
+            return Response({'error': 'this code not exist or invalid'}, status=status.HTTP_404_NOT_FOUND)
         if serializer.is_valid():
-            if OTPCode.objects.filter(code=data['code'], email=data['email']).exists():
+            clean_data = serializer.validated_data
+            if otp_obj.verify_otp(otp=clean_data['code'], email=data['email']):
                 user = User.objects.create_user(
                     email=data['email'], full_name=data['full_name'], password=data['password'])
 
                 result = serializer.save(validated_data=user)
-                otp.delete()
                 return Response(result, status=status.HTTP_201_CREATED)
-            return Response({'error': 'this code not exist'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'this code not exist or invalid'}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
--- accounts/otp_service.py
+++ accounts/otp_service.py
@@ -1,24 +1,37 @@
-import random
 import string
+from random import randint
+from django.utils import timezone
 from django.core.cache import cache
 import datetime
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
 
 
 class OTP:
 
     def __init__(self):
-        self.otp_expiry_minutes = 2
+        self.otp_expiry_minutes = 1
 
     def generate_otp(self, email):
-        otp = ''.join([random.choice(string.digits) for n in range(6)])
-        cache.set(email, otp, self.otp_expiry_minutes * 60)
+        otp = randint(1000, 9999)
+        cache.set(email, (otp, datetime.datetime.now()), self.otp_expiry_minutes * 60)
+        self.send_otp(otp, email)
         print(otp)
+
         return otp
 
     def verify_otp(self, otp, email):
-        stored_otp = cache.get(email)
-        if stored_otp == otp:
-            return True
+        stored_otp_info = cache.get(email)
+        if stored_otp_info is None:
+            return False
+        stored_otp, timestamp = stored_otp_info
+        if stored_otp == int(otp):
+            if datetime.datetime.now() - timestamp > datetime.timedelta(minutes=self.otp_expiry_minutes):
+                self.clear_otp(email)
+                return False
+            else:
+                self.clear_otp(email)
+                return True
         else:
             return False
 
@@ -26,6 +39,13 @@ class OTP:
         cache.delete(email)
 
     def send_otp(self, otp, email):
-        # Send OTP to the email address
-
-        pass
+        mail_subject = 'فعال سازی اکانت'
+        message = render_to_string('accounts/active_email.html', {
+            'user': email,
+            'code': otp,
+        })
+        to_email = email
+        email = EmailMessage(
+            mail_subject, message, to=[to_email]
+        )
+        email.send()
